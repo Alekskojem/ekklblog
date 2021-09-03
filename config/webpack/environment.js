913802a6f5f648f83818257082cb24160ee257ec
alekskojem <alekskojem@gmail.com>
2021-09-03 19:01:42 +0300

    Enviremont configs

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = reguire('webpack')
+
+environment.plugins.append(
+	"Provide",
+	new webpack.PvidePlagin((
+		$: 'jquery',
+		jQuery: 'jquery',
+		Popper: ['popper.js', 'default']
+		))
+	);
+
 module.exports = environment
