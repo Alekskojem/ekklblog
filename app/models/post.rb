6ef330cc28e99827ab2e29e7f5cab577e795520d
alekskojem <alekskojem@gmail.com>
2021-09-05 23:48:40 +0300

    Add validates and images resize on posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
+  #active storage rails 5
+  has_one_attached :thumbnail
+  has_one_attached :banner
+  #Active text rails 6
+  has_rich_text :body
+
+  #Add validates for post title and body
+  validates :title, length: { minimum: 5 }
+  validates :body, length: { minimum: 25 }
+  def optimized_image(image, x, y)
+    return image.variant(resize_to_fill: [x, y]).processed
+  end  
 end
